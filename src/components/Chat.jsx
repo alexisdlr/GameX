ec2a6761def766bac39a09100f4de89c53b808f1
Alexis De Leon <alexisdlr3009@hotmail.com>
2022-09-21 23:54:20 -0500

    Added: Chat messenger

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,11 @@
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+import MessengerCustomerChat from 'react-messenger-customer-chat'
 
 
 const Chat = ( ) => {
   return(
     <MessengerCustomerChat 
     pageId="100085535725822"
-    appId="632376898232154"
+    appId="422074486705353"
     />
   )
 }
